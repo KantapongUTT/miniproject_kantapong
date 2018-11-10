cd0f11842ed8b7c13234d7b16c12b86ba9dd612d
Chollada Narongdechpaibool <18th03@gmail.com>
2018-11-10 13:59:52 +0700

    Initial Commit

---
+++ Mini Pro 590243/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mini Pro 590243
+//
+//  Created by ict on 10/11/2561 BE.
+//  Copyright © 2561 kantapongUttarawichien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
